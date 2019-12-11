63e0c2a432c85a2008dd195efad2a3d68e4d5233
mikkie <lmj3648xm@hotmail.com>
2019-12-11 21:43:18 +0800

    update

--- tmp/result.js
+++ tmp/result.js
@@ -1,5 +1,5 @@
 db.mocktrades.insertMany([{
-    "code" : "600093",
+    "code" : "002460",
     "state" : "未开始",
     "result" : "无",
     "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
@@ -7,29 +7,33 @@ db.mocktrades.insertMany([{
     "createDate" : new Date(),
     "deleted" : false,
     "params" : {
-        "code" : "600093",
-        "volume" : "100",
-        "executeDate" : "2019-12-05",
+        "code" : "002460",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
         "monitorTime" : {
             "start" : "09:30",
-            "end" : "15:00"
+            "end" : "14:30"
         },
         "index_percent" : {
-            "low" : "-1.0",
-            "high" : "3.0"
+            "low" : "-0.3",
+            "high" : "1.5"
         },
         "percent" : {
-            "low" : "-0.5",
-            "high" : "2.5"
+            "low" : "-1",
+            "high" : "2.0"
         },
         "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
             "time" : "2",
-            "percent" : "2",
-            "amount" : "300"
+            "amount" : "500"
         }
     }
 },{
-    "code" : "002842",
+    "code" : "600459",
     "state" : "未开始",
     "result" : "无",
     "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
@@ -37,25 +41,369 @@ db.mocktrades.insertMany([{
     "createDate" : new Date(),
     "deleted" : false,
     "params" : {
-        "code" : "002842",
-        "volume" : "100",
-        "executeDate" : "2019-12-05",
+        "code" : "600459",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
         "monitorTime" : {
             "start" : "09:30",
-            "end" : "15:00"
+            "end" : "14:30"
         },
         "index_percent" : {
-            "low" : "-1.0",
-            "high" : "3.0"
+            "low" : "-0.3",
+            "high" : "1.5"
         },
         "percent" : {
-            "low" : "-0.5",
-            "high" : "2.5"
+            "low" : "-1",
+            "high" : "2.0"
         },
         "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
             "time" : "2",
-            "percent" : "2",
-            "amount" : "300"
+            "amount" : "500"
+        }
+    }
+},{
+    "code" : "002149",
+    "state" : "未开始",
+    "result" : "无",
+    "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
+    "strategyId" : ObjectId("5da19b7d181fc3600c5544c4"),
+    "createDate" : new Date(),
+    "deleted" : false,
+    "params" : {
+        "code" : "002149",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
+        "monitorTime" : {
+            "start" : "09:30",
+            "end" : "14:30"
+        },
+        "index_percent" : {
+            "low" : "-0.3",
+            "high" : "1.5"
+        },
+        "percent" : {
+            "low" : "-1",
+            "high" : "2.0"
+        },
+        "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
+            "time" : "2",
+            "amount" : "500"
+        }
+    }
+},{
+    "code" : "603305",
+    "state" : "未开始",
+    "result" : "无",
+    "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
+    "strategyId" : ObjectId("5da19b7d181fc3600c5544c4"),
+    "createDate" : new Date(),
+    "deleted" : false,
+    "params" : {
+        "code" : "603305",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
+        "monitorTime" : {
+            "start" : "09:30",
+            "end" : "14:30"
+        },
+        "index_percent" : {
+            "low" : "-0.3",
+            "high" : "1.5"
+        },
+        "percent" : {
+            "low" : "-1",
+            "high" : "2.0"
+        },
+        "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
+            "time" : "2",
+            "amount" : "500"
+        }
+    }
+},{
+    "code" : "300663",
+    "state" : "未开始",
+    "result" : "无",
+    "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
+    "strategyId" : ObjectId("5da19b7d181fc3600c5544c4"),
+    "createDate" : new Date(),
+    "deleted" : false,
+    "params" : {
+        "code" : "300663",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
+        "monitorTime" : {
+            "start" : "09:30",
+            "end" : "14:30"
+        },
+        "index_percent" : {
+            "low" : "-0.3",
+            "high" : "1.5"
+        },
+        "percent" : {
+            "low" : "-1",
+            "high" : "2.0"
+        },
+        "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
+            "time" : "2",
+            "amount" : "500"
+        }
+    }
+},{
+    "code" : "300468",
+    "state" : "未开始",
+    "result" : "无",
+    "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
+    "strategyId" : ObjectId("5da19b7d181fc3600c5544c4"),
+    "createDate" : new Date(),
+    "deleted" : false,
+    "params" : {
+        "code" : "300468",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
+        "monitorTime" : {
+            "start" : "09:30",
+            "end" : "14:30"
+        },
+        "index_percent" : {
+            "low" : "-0.3",
+            "high" : "1.5"
+        },
+        "percent" : {
+            "low" : "-1",
+            "high" : "2.0"
+        },
+        "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
+            "time" : "2",
+            "amount" : "500"
+        }
+    }
+},{
+    "code" : "300377",
+    "state" : "未开始",
+    "result" : "无",
+    "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
+    "strategyId" : ObjectId("5da19b7d181fc3600c5544c4"),
+    "createDate" : new Date(),
+    "deleted" : false,
+    "params" : {
+        "code" : "300377",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
+        "monitorTime" : {
+            "start" : "09:30",
+            "end" : "14:30"
+        },
+        "index_percent" : {
+            "low" : "-0.3",
+            "high" : "1.5"
+        },
+        "percent" : {
+            "low" : "-1",
+            "high" : "2.0"
+        },
+        "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
+            "time" : "2",
+            "amount" : "500"
+        }
+    }
+},{
+    "code" : "300085",
+    "state" : "未开始",
+    "result" : "无",
+    "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
+    "strategyId" : ObjectId("5da19b7d181fc3600c5544c4"),
+    "createDate" : new Date(),
+    "deleted" : false,
+    "params" : {
+        "code" : "300085",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
+        "monitorTime" : {
+            "start" : "09:30",
+            "end" : "14:30"
+        },
+        "index_percent" : {
+            "low" : "-0.3",
+            "high" : "1.5"
+        },
+        "percent" : {
+            "low" : "-1",
+            "high" : "2.0"
+        },
+        "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
+            "time" : "2",
+            "amount" : "500"
+        }
+    }
+},{
+    "code" : "603885",
+    "state" : "未开始",
+    "result" : "无",
+    "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
+    "strategyId" : ObjectId("5da19b7d181fc3600c5544c4"),
+    "createDate" : new Date(),
+    "deleted" : false,
+    "params" : {
+        "code" : "603885",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
+        "monitorTime" : {
+            "start" : "09:30",
+            "end" : "14:30"
+        },
+        "index_percent" : {
+            "low" : "-0.3",
+            "high" : "1.5"
+        },
+        "percent" : {
+            "low" : "-1",
+            "high" : "2.0"
+        },
+        "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
+            "time" : "2",
+            "amount" : "500"
+        }
+    }
+},{
+    "code" : "601021",
+    "state" : "未开始",
+    "result" : "无",
+    "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
+    "strategyId" : ObjectId("5da19b7d181fc3600c5544c4"),
+    "createDate" : new Date(),
+    "deleted" : false,
+    "params" : {
+        "code" : "601021",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
+        "monitorTime" : {
+            "start" : "09:30",
+            "end" : "14:30"
+        },
+        "index_percent" : {
+            "low" : "-0.3",
+            "high" : "1.5"
+        },
+        "percent" : {
+            "low" : "-1",
+            "high" : "2.0"
+        },
+        "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
+            "time" : "2",
+            "amount" : "500"
+        }
+    }
+},{
+    "code" : "300346",
+    "state" : "未开始",
+    "result" : "无",
+    "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
+    "strategyId" : ObjectId("5da19b7d181fc3600c5544c4"),
+    "createDate" : new Date(),
+    "deleted" : false,
+    "params" : {
+        "code" : "300346",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
+        "monitorTime" : {
+            "start" : "09:30",
+            "end" : "14:30"
+        },
+        "index_percent" : {
+            "low" : "-0.3",
+            "high" : "1.5"
+        },
+        "percent" : {
+            "low" : "-1",
+            "high" : "2.0"
+        },
+        "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
+            "time" : "2",
+            "amount" : "500"
+        }
+    }
+},{
+    "code" : "002815",
+    "state" : "未开始",
+    "result" : "无",
+    "userId" : ObjectId("5d905db9fc84d3224b0eb59c"),
+    "strategyId" : ObjectId("5da19b7d181fc3600c5544c4"),
+    "createDate" : new Date(),
+    "deleted" : false,
+    "params" : {
+        "code" : "002815",
+        "volume" : "1000000",
+        "executeDate" : "2019-12-12",
+        "monitorTime" : {
+            "start" : "09:30",
+            "end" : "14:30"
+        },
+        "index_percent" : {
+            "low" : "-0.3",
+            "high" : "1.5"
+        },
+        "percent" : {
+            "low" : "-1",
+            "high" : "2.0"
+        },
+        "speed" : {
+            "min_vibration" : "0.5",
+            "max_vibration" : "2.5",
+            "upper_shadow" : "0.2",
+            "ratio_l" : "2",
+            "ratio_r" : "3",
+            "time" : "2",
+            "amount" : "500"
         }
     }
 }])
